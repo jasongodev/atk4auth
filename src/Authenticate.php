f1e22f6b0b7bce7924a5d59c3f628b09315d45e7
sirjasongo <jasongo@sparkresearch.net>
2020-02-01 22:04:47 +0000

    psr errors

--- src/Authenticate.php
+++ src/Authenticate.php
@@ -1,3 +1,17 @@
 <?php
+namespace sirjasongo\atk4auth;
+
+class Authenticate
+{
+    public function __construct($app, $user, $config = [])
+    {
+        ob_start();
+        var_dump($user);
+	$data = ob_get_contents();
+	ob_clean();
+	$app->add(['View', 'template' => new \atk4\ui\Template($data)]);
+    }
+
+}
 
 ?>
